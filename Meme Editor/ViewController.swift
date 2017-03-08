3ef80f829ad8892e9ed819fe68dad6f0a62fe5da
Beau <beaugalbraith@gmail.com>
2017-03-08 14:47:19 -0600

    Initial Commit

---
+++ Meme Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme Editor
+//
+//  Created by gem on 3/8/17.
+//  Copyright © 2017 gem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
